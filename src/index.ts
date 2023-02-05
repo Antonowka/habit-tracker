a4271ed07ee7b491c467a52c6477e249b7113ed7
Anton <102463034+Antonowka@users.noreply.github.com>
2023-02-05 14:53:02 +0100

    feat: import module. Import worked

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 import './styles/normalize.css';
+
+import { calendar } from './modules/calendar';
+
+calendar();
---
+++ src/modules/calendar.ts
@@ -0,0 +1,3 @@
+export const calendar = (): void => {
+    console.log('hello');
+};
