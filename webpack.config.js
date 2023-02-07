fe87e2913e6a9eef5af618b5698b8da7504ddcca
max7394 <mlysenko92@gmail.com>
2023-02-07 14:24:37 +0300

    feat: work with assets

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const EslintPlugin = require('eslint-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.ts'),
@@ -33,6 +34,9 @@ const baseConfig = {
         }),
         new CleanWebpackPlugin(),
         new EslintPlugin({ extensions: 'ts' }),
+        new CopyPlugin({
+            patterns: [{ from: path.resolve(__dirname, 'src/assets/'), to: './assets/' }],
+        }),
     ],
 };
 
