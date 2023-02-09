4dde443e6bac5821fd9c35e967d71676a64d0484
max7394 <mlysenko92@gmail.com>
2023-02-09 09:57:53 +0300

    feat: add button-create habit to page

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './styles/main.css';
 
 import { renderCalendar, curMonths, curYears } from './modules/calendar/calendar';
+import { insertAddBtn } from './modules/habit/habit';
 
 renderCalendar(curMonths, curYears);
+insertAddBtn();
