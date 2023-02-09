1fdd74316515a149ce0536bca3e68ef87dc1c7d7
max7394 <mlysenko92@gmail.com>
2023-02-09 09:56:48 +0300

    feat: add possibility to create habit

---
+++ src/modules/habit/habit.ts
@@ -0,0 +1,103 @@
+import { clickBtn } from './modalHabit';
+
+const divBtn = document.createElement('button');
+divBtn.className = 'btn-new-habit';
+divBtn.innerHTML = '+ New Habit';
+const calendarDiv = document.querySelector('.calendar');
+divBtn.addEventListener('click', clickBtn);
+
+export function insertAddBtn() {
+    calendarDiv?.after(divBtn);
+}
+
+export function createRow() {
+    const tableBody = document.querySelector('.table-body');
+    const newRow = document.createElement('tr');
+
+    if (Array.from((tableBody as HTMLElement).childNodes).length === 1) {
+        newRow.className = 'row-1';
+        tableBody?.append(newRow);
+    } else {
+        const arrRows = (tableBody as HTMLElement).children;
+        const rowName = arrRows[arrRows.length - 1].className;
+        const rowNumber = Number(rowName.slice(4));
+
+        newRow.className = `row-${rowNumber + 1}`;
+        tableBody?.append(newRow);
+    }
+
+    // get qty day of month **********
+    const allDayOfMonth = document.querySelectorAll('.body-cell');
+    const arrayAllDayOfMonth = Array.from(allDayOfMonth);
+    const daysOfMonth = [];
+
+    arrayAllDayOfMonth.forEach((e) => {
+        if (e.id.length > 0) {
+            daysOfMonth.push(1);
+        }
+    });
+    // *************
+
+    const cellTableHabit = document.createElement('td');
+    cellTableHabit.className = `td-habit`;
+    (document.querySelector('.table-body')?.lastElementChild as HTMLElement).append(cellTableHabit);
+
+    for (let i = 1; i < daysOfMonth.length + 1; i++) {
+        const cellTable = document.createElement('td');
+        cellTable.className = `td-${i}`;
+        cellTable.addEventListener('click', coloredTd);
+        (document.querySelector('.table-body')?.lastElementChild as HTMLElement).append(cellTable);
+    }
+
+    const cellTableGoal = document.createElement('td');
+    cellTableGoal.className = `td-goal`;
+    (document.querySelector('.table-body')?.lastElementChild as HTMLElement).append(cellTableGoal);
+
+    const cellTableAchieved = document.createElement('td');
+    cellTableAchieved.className = `td-achieved`;
+    cellTableAchieved.innerHTML = '0';
+    (document.querySelector('.table-body')?.lastElementChild as HTMLElement).append(cellTableAchieved);
+}
+
+export function fillRow() {
+    const inputHabitName = (document.getElementById('modal-name-input') as HTMLInputElement).value;
+    const inputHabitGoal = (document.getElementById('modal-goal-input') as HTMLInputElement).value;
+
+    const tdHabit = document.querySelector('.table-body')?.lastElementChild?.firstElementChild;
+    const tdRow = document.querySelector('.table-body')?.lastElementChild?.childNodes;
+    const tdGoal = (tdRow as NodeList)[(tdRow as NodeList).length - 2];
+
+    (tdHabit as HTMLElement).innerHTML = `${inputHabitName}`;
+    (tdGoal as HTMLElement).innerHTML = `${inputHabitGoal}`;
+}
+
+export function coloredTd(e: Event) {
+    (e.target as HTMLElement).classList.toggle('td-colored');
+
+    if ((e.target as HTMLElement).innerHTML === '') {
+        (e.target as HTMLElement).innerHTML = '+';
+    } else {
+        (e.target as HTMLElement).innerHTML = '';
+    }
+
+    countAchieved(e);
+}
+
+export function countAchieved(e: Event) {
+    const parentEl = (e.target as HTMLElement).parentElement;
+    const tdAchieved = parentEl?.lastElementChild;
+    const allColoredTd = document.querySelectorAll('.td-colored');
+    const allColoredTdArray = Array.prototype.slice.call(allColoredTd);
+    const countElements = allColoredTdArray.filter((el) => el.parentElement === parentEl);
+
+    const allTdGoal = parentEl?.childNodes;
+    const tdGoal = (allTdGoal as NodeList)[(allTdGoal as NodeList).length - 2];
+
+    (tdAchieved as HTMLElement).innerHTML = `${countElements.length}`;
+
+    if (Number(tdAchieved?.textContent) >= Number(tdGoal.textContent)) {
+        tdAchieved?.classList.add('td-complete');
+    } else {
+        tdAchieved?.classList.remove('td-complete');
+    }
+}
