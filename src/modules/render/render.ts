3ce6d923a140594963ed74098622cbeef285d84c
Anton <102463034+Antonowka@users.noreply.github.com>
2023-02-15 21:53:44 +0100

    feat: added swich theme

--- src/modules/render/render.ts
+++ src/modules/render/render.ts
@@ -2,10 +2,12 @@ import { curMonths, curYears, renderCalendar } from '../calendar/calendar';
 import { calendarDiv, insertAddBtn } from '../habit/habit';
 import { nodsHeader } from '../nodes/headerNode';
 import { nodsFooter } from '../nodes/footerNode';
+import { switchTheme } from '../themes/theme';
 
 export const renderMainPage = () => {
     nodsHeader();
     renderCalendar(curMonths, curYears);
     calendarDiv?.after(insertAddBtn());
     nodsFooter();
+    switchTheme();
 };
