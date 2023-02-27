26fdb0c7781d117a4f5cdf1c668a591ccdf10b99
max7394 <mlysenko92@gmail.com>
2023-02-28 00:31:28 +0300

    refactor: clean main-branch

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import './styles/main.css';
-
-import { renderCalendar, curMonths, curYears } from './modules/calendar/calendar';
-import { insertAddBtn, calendarDiv } from './modules/habit/habit';
-
-renderCalendar(curMonths, curYears);
-calendarDiv?.after(insertAddBtn());
